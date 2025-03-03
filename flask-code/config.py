56484d63f0b7d76470d2a0e245f742d8e3d622a1
biao994 <zhengweibiao37@gmail.com>
2025-03-03 15:59:05 +0800

    修改告警配置

--- flask-code/config.py
+++ flask-code/config.py
@@ -8,5 +8,5 @@ class Config:
     
     # 钉钉告警配置：设置钉钉机器人 Webhook URL
     ALERT_CONFIG = {
-        "dingtalk_webhook_url": os.environ.get("DINGTALK_WEBHOOK_URL", "https://oapi.dingtalk.com/robot/send?access_token=073b0fd30d527a39bdceaecba1b1d0f7c88058ed2c4fe934826f39db0d6fc602")
+        "dingtalk_webhook_url": os.environ.get("DINGTALK_WEBHOOK_URL", "dingding_webhook_url")
     }
